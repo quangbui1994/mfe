01d603a16b6a24cd68ee981cdafe2e3d009ddd6a
Quang Bui <buiphuquang2412@gmail.com>
2021-11-28 22:14:24 +0700

    change app 7

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -15,7 +15,7 @@ const prodConfig = {
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
-                marketing: `marketing@${domain}/marketing/remoteEntry.js`
+                marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`
             },
             shared: packageJson.dependencies
         })
