7e740c7225e038c56ccb97909f789ee6dc0d6b1f
Quang Bui <buiphuquang2412@gmail.com>
2021-11-28 16:41:40 +0700

    fix webpack file

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,7 +8,8 @@ const domain = process.env.PRODUCTION_DOMAIN
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js'
+        filename: '[name].[contenthash].js',
+        publicPath: '/container/latest'
     },
     plugins: [
         new ModuleFederationPlugin({
